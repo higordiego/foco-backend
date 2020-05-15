91eca00148a7374d16360c1c028824896522908d
higordiego <higordiegoti@gmail.com>
2020-05-15 07:49:38 -0300

    fix: send email

--- src/presenters/mailgun.js
+++ src/presenters/mailgun.js
@@ -4,5 +4,5 @@ const returnPromise = (resolve, reject) => (err, body) => err ? reject(err) : re
 
 exports.sendEmail = (data, html) => new Promise((resolve, reject) => {
   const mailgun = new Mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN })
-  mailgun.messages().send({ from: data.from, to: data.email, subject: data.submission, html }, returnPromise(resolve, reject))
+  mailgun.messages().send({ from: process.env.MAILGUN_EMAIL, to: data.email, subject: data.submission, html }, returnPromise(resolve, reject))
 })
